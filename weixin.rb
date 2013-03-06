e47f6c75639019fc2902edc7b99f2e094a0ffe83
snfang <snfang@biencloud.com>
2013-03-06 09:52:01 +0800

    init

---
+++ weixin.rb
@@ -0,0 +1,44 @@
+#encoding: utf-8
+require 'sinatra'
+require 'weixin/middleware'
+require 'weixin/model'
+
+use Weixin::Middleware, 'your api token', '/your_app_root' 
+
+configure do
+    set :wx_id, 'your_weixin_account'
+end
+
+helpers do
+
+    def create_message(from, to, type, content, flag=0)
+        msg = Weixin::TextReplyMessage.new
+        msg.ToUserName = to
+        msg.FromUserName = from
+        msg.Content = content
+        msg.to_xml
+    end
+end
+
+get '/your_app_root' do
+    params[:echostr]
+end
+
+post '/your_app_root' do
+    content_type :xml, 'charset' => 'utf-8'
+
+    message = request.env[Weixin::Middleware::WEIXIN_MSG]
+    logger.info "message: #{request.env[Weixin::Middleware::WEIXIN_MSG_RAW]}"
+
+    from = message.FromUserName
+    if message.class == Weixin::TextMessage
+        content = message.Content
+        if content == 'Hello2BizUser'
+            reply_msg_content = ".....#{reply_msg_content}"
+        end
+    end
+
+    create_message(settings.wx_id, from, 'text', reply_msg_content)
+end
+
+
