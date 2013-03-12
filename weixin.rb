25f26e62f3564428da7ab380ed10bf33cecc5862
snfang <snfang@biencloud.com>
2013-03-12 11:43:16 +0800

    is ok

--- weixin.rb
+++ weixin.rb
@@ -3,11 +3,11 @@ require 'sinatra'
 require 'weixin/middleware'
 require 'weixin/model'
 
-use Weixin::Middleware, 'your api token', '/your_app_root' 
+
+use Weixin::Middleware, 'your_token', '/' 
 
 configure do
-    set :wx_id, 'your_weixin_account'
-set :public_folder, File.dirname(__FILE__) + '/bootstrap'
+    set :wx_id, 'your_id'
 end
 
 helpers do
@@ -21,14 +21,12 @@ helpers do
     end
 end
 
-get '/your_app_root' do
+get '/' do
     params[:echostr]
 end
 
-get '/' do
- erb :index
-end
-post '/your_app_root' do
+
+post '/' do
     content_type :xml, 'charset' => 'utf-8'
 
     message = request.env[Weixin::Middleware::WEIXIN_MSG]
@@ -38,11 +36,12 @@ post '/your_app_root' do
     if message.class == Weixin::TextMessage
         content = message.Content
         if content == 'Hello2BizUser'
-            reply_msg_content = ".....#{reply_msg_content}"
+            reply_msg_content = "Thx, #{reply_msg_content}"
         end
     end
 
     create_message(settings.wx_id, from, 'text', reply_msg_content)
 end
+
 
 
